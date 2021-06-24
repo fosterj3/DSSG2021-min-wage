4ef2ec4bf89f8ff8c36d95ff7292db545d7a197a
Lamar <jlfoster116@gmail.com>
2021-06-24 15:56:46 -0700

    visualized shape data by race

--- sandbox/wa_state_shapefiles.R
+++ sandbox/wa_state_shapefiles.R
@@ -1,5 +1,7 @@
 library(tidyverse)
 library(sf)
+library(RColorBrewer)
+
 
 # create a couple temp files
 temp <- tempfile()
@@ -24,9 +26,9 @@ names(census_tract_10_sf)
 glimpse(census_tract_10_sf)
 
 # subset to what we want
-# King = 003
-# Pierce = 061
-# Snohomish = 053
+# King = 033
+# Snohomish = 061
+# Pierce = 053
 project_county_fp <- c("033", "061", "053")
 
 # only get tracts we need for the project
@@ -35,4 +37,25 @@ sub_tracts <- census_tract_10_sf %>%
 
 # visualize tracts
 ggplot() +
-  geom_sf(data = sub_tracts)
+  geom_sf(data = sub_tracts,aes(fill = POP10)) + 
+  scale_fill_distiller(palette = "Purples", trans = "reverse") + 
+  labs(fill = "population", title = "Overall population in Snohomish, King and Pierce County", subtitle = "Washington State") +
+  theme_void()
+
+
+
+#Wrangle Data to make it Long - doing this for Race columns  
+sub_tract_long <- pivot_longer(sub_tracts, cols = POPWHITE:POPTWO2, names_to = "race", values_to = "race_value")
+sub_tract_long <- sub_tract_long %>% mutate(race = str_remove(race,".*POP" ))
+sub_tract_long <- sub_tract_long %>% mutate(race = str_remove(race,"\\d")) #I'm not sure what the digits mean
+sub_tract_long <- sub_tract_long %>% mutate(race = str_to_title(race))
+
+
+# visualize tracts by race 
+ggplot() +
+  geom_sf(data = sub_tract_long,aes(fill = race_value, geometry = geometry)) + 
+  scale_fill_distiller(palette = "Spectral", trans = "reverse") + 
+  labs(fill = "population", title = "Overall population in Snohomish, King and Pierce County", subtitle = "Washington State") +
+  theme_void() +
+  facet_wrap(~race)
+
